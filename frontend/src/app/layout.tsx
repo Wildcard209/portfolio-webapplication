ea83235ec95890e41039e71323b8f291a6d5d71f
Jessica Wylde <jess@jessicawylde.co.uk>
2025-06-23 01:38:17 +0100

    Added google analytics

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -15,6 +15,8 @@ const roboto = localFont({
   variable: "--font-roboto",
 });
 
+const googleAnalyticsId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID ?? "";
+
 export const metadata: Metadata = {
   title: "Jessica Wylde",
   description: "Jessica Wyldes portfolio page",
@@ -27,32 +29,51 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${roboto.variable} ${peanutButter.variable}`}>
-      <head>
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/assets/favicons/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/assets/favicons/favicon-16x16.png"
-        />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
-        <meta name="theme-color" content="#ffffff" />
-        <meta name="description" content="Jessica Wylde" />
-        <meta name="author" content="Jessica Wylde" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/assets/favicons/apple-touch-icon.png"
-        />
-        <link rel="manifest" href="/assets/favicons/site.webmanifest" />
+<head>
+
+  {/* Google Analytics */}
+  {/* eslint-disable-next-line @next/next/no-sync-scripts */}
+  {googleAnalyticsId && (
+    <>
+      <script async src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`}></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${googleAnalyticsId}');
+          `,
+        }}
+      />
+    </>
+  )}
+
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="32x32"
+    href="/assets/favicons/favicon-32x32.png"
+  />
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="16x16"
+    href="/assets/favicons/favicon-16x16.png"
+  />
+  <meta
+    name="viewport"
+    content="width=device-width, initial-scale=1, shrink-to-fit=no"
+  />
+  <meta name="theme-color" content="#ffffff" />
+  <meta name="description" content="Jessica Wylde" />
+  <meta name="author" content="Jessica Wylde" />
+  <link
+    rel="apple-touch-icon"
+    sizes="180x180"
+    href="/assets/favicons/apple-touch-icon.png"
+  />
+  <link rel="manifest" href="/assets/favicons/site.webmanifest" />
         <title>{`${metadata.title}`}</title>
       </head>
       <body>
